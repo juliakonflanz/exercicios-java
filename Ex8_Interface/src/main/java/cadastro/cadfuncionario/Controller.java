99290f032b6a3a075d43effb75cd1825e84328ee
Júlia Konflanz <84208213+juliakonflanz@users.noreply.github.com>
2021-12-13 23:08:31 -0300

    Add files via upload
    Upload da pasta com o desenvolvimento da interface ainda em construção.

---
+++ Ex8_Interface/src/main/java/cadastro/cadfuncionario/Controller.java
@@ -0,0 +1,14 @@
+package cadastro.cadfuncionario;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Testando JavaFX");
+    }
+}
