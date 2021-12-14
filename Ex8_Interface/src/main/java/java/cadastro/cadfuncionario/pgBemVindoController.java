bb8fe5060fc8198d088c533f0510d71f75d7748f
Júlia Konflanz <84208213+juliakonflanz@users.noreply.github.com>
2021-12-14 10:54:47 -0300

    Add files via upload

---
+++ Ex8_Interface/src/main/java/java/cadastro/cadfuncionario/pgBemVindoController.java
@@ -0,0 +1,54 @@
+package cadastro.cadfuncionario;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class pgBemVindoController {
+
+    @FXML
+    private Button BtnSair;
+
+    @FXML
+    private Button btnFuncionarios;
+
+    @FXML
+    private Button btnNovoCadastro;
+
+    @FXML
+    private Label labelTxt1;
+
+    @FXML
+    private Label labelTxt2;
+
+    @FXML
+    private Label labelTxt3;
+
+    @FXML
+    private Pane paneBemVindo;
+
+    @FXML
+    private Pane paneiCad;
+
+    @FXML
+    private VBox vboxMenu;
+
+    @FXML
+    void btnFuncionariosOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNovoCadastroOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSairOnAction(ActionEvent event) {
+
+    }
+
+}
