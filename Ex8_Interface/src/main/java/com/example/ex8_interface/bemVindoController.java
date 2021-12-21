e0782120af017e871f5799377a9ede3983a210bf
Júlia Konflanz <84208213+juliakonflanz@users.noreply.github.com>
2021-12-20 21:53:51 -0300

    Update bemVindoController.java

--- Ex8_Interface/src/main/java/com/example/ex8_interface/bemVindoController.java
+++ Ex8_Interface/src/main/java/com/example/ex8_interface/bemVindoController.java
@@ -1,31 +1,36 @@
-package com.example.ex8_interface;
+package com.example.ex8_interfacecadastro;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.Node;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class bemVindoController {
+public class bemVindoController implements Initializable {
 
     private Stage stage;
     private Scene scene;
 
-    @FXML
-    void btnFuncionariosOnAction(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("listaFuncionarios.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
+    @FXML private Button btnFuncionarios;
+    @FXML private Button btnNovoCadastro;
+    @FXML private Button btnSair;
 
     @FXML
-    void btnNovoCadastroOnAction(ActionEvent event) throws IOException {
+    void initialize() {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    @FXML void btnNovoCadastroOn(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("novoCadastro.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -33,8 +38,15 @@ public class bemVindoController {
         stage.show();
     }
 
-    @FXML
-    void btnSairOnAction(ActionEvent event) {
+    @FXML void btnFuncionariosOn(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("funcionarios.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML void btnSairOn(ActionEvent event) {
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.close();
     }
