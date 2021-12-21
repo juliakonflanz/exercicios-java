d539ad42bfa11e8f4e2cdbcecbf5011de08d7884
Júlia Konflanz <84208213+juliakonflanz@users.noreply.github.com>
2021-12-20 21:55:13 -0300

    Update novoCadastroController.java

--- Ex8_Interface/src/main/java/com/example/ex8_interface/novoCadastroController.java
+++ Ex8_Interface/src/main/java/com/example/ex8_interface/novoCadastroController.java
@@ -1,35 +1,42 @@
-package com.example.ex8_interface;
+package com.example.ex8_interfacecadastro;
 
-import algoritmos.Funcionario;
+import listaEncadeada.Funcionario;
+import listaEncadeada.Lista;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
-import java.util.LinkedList;
-import java.util.List;
-import static java.lang.Integer.parseInt;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import java.time.LocalDate;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
 
 public class novoCadastroController {
 
     private Stage stage;
     private Scene scene;
-    private final List<Funcionario> listaFuncionarios = new LinkedList<>();
 
-    @FXML private TextField txtCodigo, txtNome, txtSalario;
-    @FXML private DatePicker txtData;
+    @FXML private Button btnCadastrar;
+    @FXML private Button btnFuncionarios;
+    @FXML private Button btnHome;
+    @FXML private Button btnNovoCadastro;
+    @FXML private Button btnSair;
+    @FXML private DatePicker dateDataAdmissao;
+    @FXML private TextField txtCodFuncionario;
+    @FXML private TextField txtNome;
+    @FXML private TextField txtValorSalario;
 
-    @FXML
-    void btniCadOnAction (ActionEvent event) throws IOException {
+    private final Lista<Funcionario> listaFuncionarios = new Lista<Funcionario>();
+
+    @FXML void btnHomeOn(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("bemVindo.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -37,44 +44,40 @@ public class novoCadastroController {
         stage.show();
     }
 
-    @FXML
-    void btnFuncionariosOnAction(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("listaFuncionarios.fxml"));
+    @FXML void btnFuncionariosOn(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("funcionarios.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
-    @FXML
-    void btnSairOnAction(ActionEvent event) {
+    @FXML void btnSairOn(ActionEvent event) {
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.close();
     }
 
-    public void limpaTxt(){
-        txtCodigo.clear();
-        txtNome.clear();
-        txtSalario.clear();
+    @FXML private void btnCadastrarOn(javafx.event.ActionEvent actionEvent) throws IOException {
+        int codFuncionario = Integer.parseInt(txtCodFuncionario.getText());
+        String nome = txtNome.getText();
+        String valorSalario = listaFuncionarios.trocaSalario(txtValorSalario.getText());
+        String dataAdmissao = dateDataAdmissao.getValue().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+        listaFuncionarios.insereNoFim(new Funcionario(codFuncionario, nome, valorSalario, dataAdmissao));
+
+        Alert cadastroOk = new Alert(AlertType.INFORMATION);
+        cadastroOk.setTitle("Aviso");
+        cadastroOk.setHeaderText("Funcionário cadastrado!");
+        cadastroOk.show();
+
+        limpaCampos();
+        listaFuncionarios.salvaArquivo();
     }
 
-    @FXML
-    void btnCadastrarOnAction(ActionEvent event) {
-        int codFuncionario = parseInt(txtCodigo.getText());
-        String nome = txtNome.getText();
-        String valorSalario = txtSalario.getText();
-        LocalDate localDate = txtData.getValue();
-        DateTimeFormatter formatoData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String dataAdmissao = localDate.format(formatoData);
-
-        Funcionario listaFuncionarios = new Funcionario(codFuncionario, nome, valorSalario, dataAdmissao);
-
-        limpaTxt();
-
-        Alert cadastroSalvo = new Alert(AlertType.INFORMATION);
-        cadastroSalvo.setTitle("AVISO");
-        cadastroSalvo.setHeaderText("Cadastro completo!");
-        cadastroSalvo.show();
+    public void limpaCampos(){
+        txtCodFuncionario.clear();
+        txtNome.clear();
+        txtValorSalario.clear();
+        dateDataAdmissao = null;
     }
 
 }
